373027678e1e91dfb8bd4d814d0c8ff8ea195585
kanggeonnim <gun4329@gmail.com>
2023-11-21 23:43:12 +0900

    update: common login

--- Back/src/main/java/com/ssafy/hotplace/controller/AccountController.java
+++ Back/src/main/java/com/ssafy/hotplace/controller/AccountController.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.hotplace.model.KakaoDTO;
@@ -19,6 +22,7 @@ import com.ssafy.hotplace.model.service.MemberService;
 import com.ssafy.hotplace.model.service.social.KakaoService;
 import com.ssafy.hotplace.util.JWTUtil;
 
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -42,44 +46,34 @@ public class AccountController {
 //  @GetMapping("/signin/kakao")
 	@GetMapping("/kakao/callback")
 	public ResponseEntity<MsgEntity> callback(HttpServletRequest request) throws Exception {
-		
+
 		KakaoDTO kakaoInfo = kakaoService.getKakaoInfo(request.getParameter("code"));
-		
+
 		Optional<MemberDTO> userInfo = memberService.loginByKakaoId(String.valueOf(kakaoInfo.getId()));
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		HttpStatus status = HttpStatus.ACCEPTED;
+
 		// 기존 회원인 경우
 		if (userInfo.isPresent()) {
-//			String accessToken = jwtUtil.createAccessToken(member.getId());
-//			String refreshToken = jwtUtil.createRefreshToken(member.getId());
-			if (jwtUtil.checkToken(request.getHeader("Authorization"))) {
-				log.info("사용 가능한 토큰!!!");
-				try {
-//					로그인 사용자 정보.
-					MemberDTO member = memberService.userInfo("kakao" + String.valueOf(kakaoInfo.getId()));
-					resultMap.put("userInfo", member);
-					status = HttpStatus.OK;
-				} catch (Exception e) {
-					log.error("정보조회 실패 : {}", e);
-					resultMap.put("message", e.getMessage());
-					status = HttpStatus.INTERNAL_SERVER_ERROR;
-				}
-			} else {
-				log.error("사용 불가능 토큰!!!");
-				status = HttpStatus.UNAUTHORIZED;
-			}
+//			로그인 사용자 정보.
+			MemberDTO member = memberService.userInfo("kakao" + String.valueOf(kakaoInfo.getId()));
+			String accessToken = jwtUtil.createAccessToken(member.getId());
+			String refreshToken = jwtUtil.createRefreshToken(member.getId());
+
+//			발급받은 refresh token을 DB에 저장.
+			memberService.saveRefreshToken(member.getId(), refreshToken);
+			resultMap.put("userInfo", member);
+
+//			JSON으로 token 전달.
+			resultMap.put("access-token", accessToken);
+			status = HttpStatus.OK;
 		}
 		// 신규 회원인 경우(userInfo가 null인경우
 		else {
-			MemberDTO member = MemberDTO.builder()
-					.id("kakao" + String.valueOf(kakaoInfo.getId()))
-					.name(String.valueOf(kakaoInfo.getNickname()))
-					.email(kakaoInfo.getEmail())
-					.role("일반 회원")
-					.provider("kakao")
-					.providerId(String.valueOf(kakaoInfo.getId()))
-					.build();
-			
+			MemberDTO member = MemberDTO.builder().id("kakao" + String.valueOf(kakaoInfo.getId()))
+					.name(String.valueOf(kakaoInfo.getNickname())).email(kakaoInfo.getEmail()).role("일반 회원")
+					.provider("kakao").providerId(String.valueOf(kakaoInfo.getId())).build();
+
 			String accessToken = jwtUtil.createAccessToken(member.getId());
 			String refreshToken = jwtUtil.createRefreshToken(member.getId());
 			log.debug("access token : {}", accessToken);
@@ -88,7 +82,7 @@ public class AccountController {
 //			발급받은 refresh token을 DB에 저장.
 //			memberService.saveRefreshToken(member.getId(), refreshToken);
 			member.setRefreshToken(refreshToken);
-			
+
 //			JSON으로 token 전달.
 			resultMap.put("access-token", accessToken);
 //			resultMap.put("refresh-token", refreshToken);
@@ -102,114 +96,110 @@ public class AccountController {
 		System.out.println(kakaoInfo.getId());
 		System.out.println(kakaoInfo.getEmail());
 		System.out.println(kakaoInfo.getNickname());
-		
+
 		return ResponseEntity.ok().body(new MsgEntity("Success", kakaoInfo));
 //		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
 
-//	@ApiOperation(value = "로그인", notes = "아이디와 비밀번호를 이용하여 로그인 처리.")
-//	@PostMapping("/login")
-//	public ResponseEntity<Map<String, Object>> login(
-//			@RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true) MemberDto memberDto) {
-//		log.debug("login user : {}", memberDto);
-//		Map<String, Object> resultMap = new HashMap<String, Object>();
-//		HttpStatus status = HttpStatus.ACCEPTED;
-//		try {
-//			MemberDTO loginUser = memberService.login(memberDto);
-//			if (loginUser != null) {
-//				String accessToken = jwtUtil.createAccessToken(loginUser.getUserId());
-//				String refreshToken = jwtUtil.createRefreshToken(loginUser.getUserId());
-//				log.debug("access token : {}", accessToken);
-//				log.debug("refresh token : {}", refreshToken);
-//
-////				발급받은 refresh token을 DB에 저장.
-//				memberService.saveRefreshToken(loginUser.getUserId(), refreshToken);
-//
-////				JSON으로 token 전달.
-//				resultMap.put("access-token", accessToken);
-//				resultMap.put("refresh-token", refreshToken);
-//
-//				status = HttpStatus.CREATED;
-//			} else {
-//				resultMap.put("message", "아이디 또는 패스워드를 확인해주세요.");
-//				status = HttpStatus.UNAUTHORIZED;
-//			}
-//
-//		} catch (Exception e) {
-//			log.debug("로그인 에러 발생 : {}", e);
-//			resultMap.put("message", e.getMessage());
-//			status = HttpStatus.INTERNAL_SERVER_ERROR;
-//		}
-//		return new ResponseEntity<Map<String, Object>>(resultMap, status);
-//	}
-//
-//	@ApiOperation(value = "회원인증", notes = "회원 정보를 담은 Token을 반환한다.", response = Map.class)
-//	@GetMapping("/info/{userId}")
-//	public ResponseEntity<Map<String, Object>> getInfo(
-//			@PathVariable("userId") @ApiParam(value = "인증할 회원의 아이디.", required = true) String userId,
-//			HttpServletRequest request) {
-////		logger.debug("userId : {} ", userId);
-//		Map<String, Object> resultMap = new HashMap<>();
-//		HttpStatus status = HttpStatus.ACCEPTED;
-//		if (jwtUtil.checkToken(request.getHeader("Authorization"))) {
-//			log.info("사용 가능한 토큰!!!");
-//			try {
-////				로그인 사용자 정보.
-//				MemberDto memberDto = memberService.userInfo(userId);
-//				resultMap.put("userInfo", memberDto);
-//				status = HttpStatus.OK;
-//			} catch (Exception e) {
-//				log.error("정보조회 실패 : {}", e);
-//				resultMap.put("message", e.getMessage());
-//				status = HttpStatus.INTERNAL_SERVER_ERROR;
-//			}
-//		} else {
-//			log.error("사용 불가능 토큰!!!");
-//			status = HttpStatus.UNAUTHORIZED;
-//		}
-//		return new ResponseEntity<Map<String, Object>>(resultMap, status);
-//	}
-//
-//	@ApiOperation(value = "로그아웃", notes = "회원 정보를 담은 Token을 제거한다.", response = Map.class)
-//	@GetMapping("/logout/{userId}")
-//	public ResponseEntity<?> removeToken(
-//			@PathVariable("userId") @ApiParam(value = "로그아웃할 회원의 아이디.", required = true) String userId) {
-//		Map<String, Object> resultMap = new HashMap<>();
-//		HttpStatus status = HttpStatus.ACCEPTED;
-//		try {
-//			memberService.deleRefreshToken(userId);
-//			status = HttpStatus.OK;
-//		} catch (Exception e) {
-//			log.error("로그아웃 실패 : {}", e);
-//			resultMap.put("message", e.getMessage());
-//			status = HttpStatus.INTERNAL_SERVER_ERROR;
-//		}
-//		return new ResponseEntity<Map<String, Object>>(resultMap, status);
-//
-//	}
-//
-//	@ApiOperation(value = "Access Token 재발급", notes = "만료된 access token을 재발급받는다.", response = Map.class)
-//	@PostMapping("/refresh")
-//	public ResponseEntity<?> refreshToken(@RequestBody MemberDto memberDto, HttpServletRequest request)
-//			throws Exception {
-//		Map<String, Object> resultMap = new HashMap<>();
-//		HttpStatus status = HttpStatus.ACCEPTED;
-//		String token = request.getHeader("refreshToken");
-//		log.debug("token : {}, memberDto : {}", token, memberDto);
-//		if (jwtUtil.checkToken(token)) {
-//			if (token.equals(memberService.getRefreshToken(memberDto.getUserId()))) {
-//				String accessToken = jwtUtil.createAccessToken(memberDto.getUserId());
-//				log.debug("token : {}", accessToken);
-//				log.debug("정상적으로 액세스토큰 재발급!!!");
-//				resultMap.put("access-token", accessToken);
-//				status = HttpStatus.CREATED;
-//			}
-//		} else {
-//			log.debug("인증시간이 만료되었습니다. 로그인이 필요합니다.");
-//			status = HttpStatus.UNAUTHORIZED;
-//		}
-//		return new ResponseEntity<Map<String, Object>>(resultMap, status);
-//	}
+	@PostMapping("/login")
+	public ResponseEntity<Map<String, Object>> login(
+			@RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true) MemberDTO member) {
+		log.debug("login user : {}", member);
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		try {
+			MemberDTO loginUser = memberService.login(member);
+			if (loginUser != null) {
+				String accessToken = jwtUtil.createAccessToken(loginUser.getId());
+				String refreshToken = jwtUtil.createRefreshToken(loginUser.getId());
+				log.debug("access token : {}", accessToken);
+				log.debug("refresh token : {}", refreshToken);
+
+//				발급받은 refresh token을 DB에 저장.
+				memberService.saveRefreshToken(loginUser.getId(), refreshToken);
+
+//				JSON으로 token 전달.
+				resultMap.put("access-token", accessToken);
+				resultMap.put("refresh-token", refreshToken);
+
+				status = HttpStatus.CREATED;
+			} else {
+				resultMap.put("message", "아이디 또는 패스워드를 확인해주세요.");
+				status = HttpStatus.UNAUTHORIZED;
+			}
+
+		} catch (Exception e) {
+			log.debug("로그인 에러 발생 : {}", e);
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+
+	@GetMapping("/info/{userId}")
+	public ResponseEntity<Map<String, Object>> getInfo(
+			@PathVariable("userId") @ApiParam(value = "인증할 회원의 아이디.", required = true) String userId,
+			HttpServletRequest request) {
+//		logger.debug("userId : {} ", userId);
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		if (jwtUtil.checkToken(request.getHeader("Authorization"))) {
+			log.info("사용 가능한 토큰!!!");
+			try {
+//				로그인 사용자 정보.
+				MemberDTO member = memberService.userInfo(userId);
+				resultMap.put("userInfo", member);
+				status = HttpStatus.OK;
+			} catch (Exception e) {
+				log.error("정보조회 실패 : {}", e);
+				resultMap.put("message", e.getMessage());
+				status = HttpStatus.INTERNAL_SERVER_ERROR;
+			}
+		} else {
+			log.error("사용 불가능 토큰!!!");
+			status = HttpStatus.UNAUTHORIZED;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+
+	@GetMapping("/logout/{userId}")
+	public ResponseEntity<?> removeToken(
+			@PathVariable("userId") @ApiParam(value = "로그아웃할 회원의 아이디.", required = true) String userId) {
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		try {
+			memberService.deleRefreshToken(userId);
+			status = HttpStatus.OK;
+		} catch (Exception e) {
+			log.error("로그아웃 실패 : {}", e);
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+
+	}
+
+	@PostMapping("/refresh")
+	public ResponseEntity<?> refreshToken(@RequestBody MemberDTO member, HttpServletRequest request)
+			throws Exception {
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		String token = request.getHeader("refreshToken");
+		log.debug("token : {}, memberDto : {}", token, member);
+		if (jwtUtil.checkToken(token)) {
+			if (token.equals(memberService.getRefreshToken(member.getId()))) {
+				String accessToken = jwtUtil.createAccessToken(member.getId());
+				log.debug("token : {}", accessToken);
+				log.debug("정상적으로 액세스토큰 재발급!!!");
+				resultMap.put("access-token", accessToken);
+				status = HttpStatus.CREATED;
+			}
+		} else {
+			log.debug("인증시간이 만료되었습니다. 로그인이 필요합니다.");
+			status = HttpStatus.UNAUTHORIZED;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
 
 //    //소셜 카카오 로그인
 //    @ApiOperation(value = "kakao login api info", notes = "For login, using kakao open api. Need to get authorization code.")
